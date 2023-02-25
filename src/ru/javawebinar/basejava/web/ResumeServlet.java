9bb3031c555ab2d10d394ca9f7401a6dd4fa04e9
maksimkonovalov <konovalo_ms@mail.ru>
2023-02-25 07:29:58 +0300

    HW_Lesson_15_25-02-2023

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,13 +1,15 @@
 package ru.javawebinar.basejava.web;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ResumeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.getWriter().write("Hello Resumes!");
     }
 
     @Override
